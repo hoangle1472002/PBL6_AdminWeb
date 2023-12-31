7754c8ea349977d8064333524a2cce08a3ab0fbf
Nguyễn Đức Quốc <ducquoc2002@gmail.com>
2023-12-31 12:42:30 +0700

    feat: config env

--- src/Apis/config.js
+++ src/Apis/config.js
@@ -1,3 +1,4 @@
-const baseUrl = "https://ec2-13-229-80-249.ap-southeast-1.compute.amazonaws.com/api/";
+// const baseUrl = "https://ec2-13-229-80-249.ap-southeast-1.compute.amazonaws.com/api/";
 // const baseUrl = "http://localhost:8080/api/";
+const baseUrl = process.env.REACT_APP_BASE_API_URL;
 export default baseUrl;
